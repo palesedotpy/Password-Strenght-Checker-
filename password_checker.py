a0dc4e7128c07fd2728c3f9c66451a5607f241a8
Palese <tdesantiss2006@gmail.com>
2026-04-11 23:05:00 +0200

    Implemented all methods

--- password_checker.py
+++ password_checker.py
@@ -1,10 +1,14 @@
+
+import re
+
 """
     Password Checker
 """
+
 class PasswordChecker():
-    def __init__(self) -> None:
+    def __init__(self, given_password) -> None:
         self.min_length = 8                     # Password's minimum length is 8, however for a strong password are reccomended at least 14 characters
-        self.valid_chars = ['!$%&()?*_@#-']
+        self.valid_chars = '!$%&()?*_@#-'
         self.banned_passwords = [                # Can be a txt list
                                     "abcdefgh",
                                     "password",
@@ -13,27 +17,48 @@ class PasswordChecker():
                                     "87654321",
                                     "iloveyou"
                                 ]
+        self.given_password = given_password
         
-    def isMinimumLength(self, password) -> bool:
-        pass
+    def isMinimumLength(self) -> bool:
+        return len(self.given_password) >= self.min_length
 
     def hasInvalidChars(self) -> bool:
-        pass
+        valid_chars_pattern = r"[^a-zA-Z0-9" + self.valid_chars + "]"
+        return bool(re.findall(valid_chars_pattern, self.given_password))
 
     def isBanned(self) -> bool:
-        pass
+        if self.given_password.lower() in self.banned_passwords:
+            return True
+        return False
 
-    def hasUppercaseLetter(self) -> bool:
-        pass
+    def findChars(self, charsToFind: str) -> bool:
+        return bool(re.search(charsToFind, self.given_password))
 
-    def hasLowercaseLetter(self) -> bool:
-        pass
+    def hasRequiredChars(self) -> bool:
+        if self.findChars(r"[A-Z]") and self.findChars(r"[a-z]") and self.findChars(r"[0-9]"):
+            valid_chars_pattern = r"[!\$%&\(\)\?\*\-_@#']"
+            if self.findChars(valid_chars_pattern):
+                return True
+        return False
 
-    def hasNumbers(self) -> bool:
-        pass
-
-    def hasSpecialChars(self) -> bool:
-        pass
-
-    def isPasswordSecure(self) -> bool:
-        pass
+    def isPasswordStrong(self) -> bool:
+        if (self.isMinimumLength()):
+            print("MinimumLength: YES")
+        else:
+            print("MinimumLength: NO")
+        
+        if (self.hasInvalidChars()):
+            print("hasInvalidChars: YES")
+        else:
+            print("hasInvalidChars: NO")
+        
+        if (self.isBanned()):
+            print("isBanned: YES")
+        else:
+            print("isBanned: NO")
+        
+        if (self.hasRequiredChars()):
+            print("hasRequiredChars: YES")
+        else:
+            print("hasRequiredChars: NO")
+        
--- password_manager.py
+++ password_manager.py
@@ -7,26 +7,34 @@ import password_checker
 class PasswordManager():
         
     def __init__(self) -> None:
-        self.input_password = ""
+        self.input_password = "Il#ov@ey9Ou"
+        self.password_checker = password_checker.PasswordChecker(self.input_password)
 
-    def getInputPassword(self) -> str:
+    def getPassword(self) -> str:
         return self.input_password
     
-    def setIputPassword(self, given_password) -> str:
+    def setPassword(self, given_password) -> str:
         self.input_password = given_password
         return "OK"
 
-    def inputPassowrd(self) -> str:
-        given_password = input("Inter your password > ")
-        
-        
-        
-
+    def inputPassword(self) -> str:
+        input_password = input("Inter your password > ")
+        self.setPassword(input_password)
 
     def __str__(self) -> str:
         return "A strong password should follow the "
     
 
+def main():
+    password_manager = PasswordManager()
+    print("Welcome in Password Checker")
+    # password_manager.inputPassword() TODO remove comment
 
+    password_manager.password_checker.isPasswordStrong()
+
+if __name__ == "__main__":
+    main()
+
+# Bibliography
 # Micorosoft: Password policy recommendations for Microsoft 365 passwords (https://learn.microsoft.com/en-us/microsoft-365/admin/misc/password-policy-recommendations?view=o365-worldwide)
 # CISA: Require Strong Passwords (https://www.cisa.gov/audiences/small-and-medium-businesses/secure-your-business/require-strong-passwords)
