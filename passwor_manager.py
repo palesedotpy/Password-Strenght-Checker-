5dfd8d78c95a0eaeb8cd117d8f80e49952128ed3
Palese <tdesantiss2006@gmail.com>
2026-04-11 19:44:54 +0200

    Password checker class moved in separate file

--- passwor_manager.py
+++ passwor_manager.py
@@ -1,49 +1,10 @@
 
+import password_checker
+
 """
     Password Manager
 """
 class PasswordManager():
-
-    """
-        Password Checker
-    """
-    class PasswordChecker():
-        def __init__(self) -> None:
-            self.min_length = 8                     # Password's minimum length is 8, however for a strong password are reccomended at least 14 characters
-            self.valid_chars = ['!$%&()?*_@#-']
-            self.banned_passwords = [                # Can be a txt list
-                                        "abcdefgh",
-                                        "password",
-                                        "monkey12",
-                                        "12345678", 
-                                        "87654321",
-                                        "iloveyou"
-                                    ]
-            
-        def isMinimumLength(self, password) -> bool:
-            pass
-
-        def hasInvalidChars(self) -> bool:
-            pass
-
-        def isBanned(self) -> bool:
-            pass
-
-        def hasUppercaseLetter(self) -> bool:
-            pass
-
-        def hasLowercaseLetter(self) -> bool:
-            pass
-
-        def hasNumbers(self) -> bool:
-            pass
-
-        def hasSpecialChars(self) -> bool:
-            pass
-
-        def isPasswordSecure(self) -> bool:
-            pass
-
         
     def __init__(self) -> None:
         self.input_password = ""
